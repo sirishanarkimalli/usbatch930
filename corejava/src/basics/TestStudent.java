30eec8375b312af1b2680d9a5d4f9808836df310
91998 <91998@192.168.0.12>
2020-02-26 10:46:00 +0530

    Program to create a class Student with multiple constructors

--- corejava/src/basics/TestStudent.java
+++ corejava/src/basics/TestStudent.java
@@ -1,45 +1,51 @@
 package basics;
 
 class Student {
+
+	private int rollnum;
+	private String name;
+	private long phoneNumber;
+
+	//Default Constructor
+	public  Student() {
+		rollnum=1;
+		name="Aran";
+		phoneNumber=9999999999L;
+	}
 	
-	 private int rollnum;
-	 private String name;
-	 private long phoneNumber;
+	//parameterized Constructor
+	public Student(int num, String nam, long phNum) {
+		rollnum = num;
+		name = nam;
+		phoneNumber = phNum;
+	}
 	
-	
-	 public void setData(int num,String nam, long phNum) {
-		 	rollnum=num;
-		 	name=nam;
-		 	phoneNumber= phNum;
-	 }
-	 
+//	public void setData(int num, String nam, long phNum) {
+//		rollnum = num;
+//		name = nam;
+//		phoneNumber = phNum;
+//	}
+
 	public void displayDetails() {
-		System.out.println("Rollnum="+rollnum);
-		System.out.println("Name="+name);
-		System.out.println("PhoneNumber="+phoneNumber);
+		System.out.println("Rollnum=" + rollnum);
+		System.out.println("Name=" + name);
+		System.out.println("PhoneNumber=" + phoneNumber);
 		System.out.println("_________________");
 	}
 
 }
 
-
-public class TestStudent{
+public class TestStudent {
 	public static void main(String args[]) {
-		Student obj1= new Student();
-		Student obj2= new Student();
-		
-		obj1.setData(01,"Rahul",9999999);
-		
-		obj2.setData(02, "sathish",888888);
-		
+		Student obj1 = new Student();
+		Student obj2 = new Student(02,"George",8888888888L);
+
+//		obj1.setData(01, "Rahul", 9999999);
+//
+//		obj2.setData(02, "sathish", 888888);
+
 		obj1.displayDetails();
 		obj2.displayDetails();
-		
+
 	}
 }
-
-
-
-
-
-

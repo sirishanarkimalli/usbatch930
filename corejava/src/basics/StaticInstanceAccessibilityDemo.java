4f4b73c18896eab8373ebe93f16a38d5374138d8
91998 <91998@192.168.0.12>
2020-03-05 10:41:09 +0530

    Program to check the accessibility of static method from instance method and vice-versa

--- corejava/src/basics/StaticInstanceAccessibilityDemo.java
+++ corejava/src/basics/StaticInstanceAccessibilityDemo.java
@@ -5,26 +5,28 @@ public class StaticInstanceAccessibilityDemo {
 //2. Calling instance method from static method.
 	
 	public void method1() {
-		System.out.println("Instance Method");
+		System.out.println("Instance Method 1");
 		method2();
 	}
 	
 	public static void method2() {
-		System.out.println("Statc method");
+		System.out.println("Static method 2");
 	}
 	
 	
 	public void method3() {
-		
+		System.out.println("Instance Method 3");
 	}
 	
 	public static void method4() {
+		System.out.println("Static method 4");
 	}
 	
 	
 	
 	public static void main(String[] args) {
-		
+			new StaticInstanceAccessibilityDemo().method3();
+			method4();
 
 	}
 

e81786dc6f2e2b065fb617b5ec59389204bbdc94
91998 <91998@192.168.0.12>
2020-03-19 19:21:18 +0530

    Implementing Interface

--- corejava/src/basics/MyStack.java
+++ corejava/src/basics/MyStack.java
@@ -1,39 +1,62 @@
 package basics;
 
-
-interface Stack{
-	public void push(int val);
-	public int pop();
+interface Stack {
+	 void push(int val);
+	 int pop();
+	 int length();
+	 
+	 default String defaultMethod() {
+		 return "Stack Default Method";
+	 }
+	 
+	 static void getName() {
+		 System.out.println("Stack Implementations");
+	 }
 }
 
-interface display{
-	
+interface Display {
+	void print();
 }
 
+public class MyStack implements Stack,Display {
+	int arr[] = new int[5];
+	int top = 0;
 
-public class MyStack implements Stack,display{
-	int arr[]=new int[5];
-	int top=0;
 	public void push(int val) {
-		arr[top++]=val;
-			
+		arr[top++] = val;
 	}
+
 	public int pop() {
-		int val=arr[--top];
-		
+		int val = arr[--top];
 		return val;
 	}
 	
+	public int length() {
+		return top;
+	}
+
+	public void print() {
+		System.out.println("Method of Display interface");
+	}
+	
 	public void display() {
-		for(int val:arr)
-			System.out.print(val+" ");
+		for (int val : arr)
+			System.out.print(val + " ");
 	}
 
 	public static void main(String[] args) {
-			MyStack m= new MyStack();
-			m.push(10);
-			m.push(20);
-			System.out.println(m.pop());
+//		An interface reference can hold an object of a class that implements the interface 
+		Stack m = new MyStack();
+		m.push(10);
+		m.push(20);
+		//call default method of interface 
+		m.defaultMethod();
+		//Static method
+		Stack.getName();
+		Display d= new MyStack();
+	    d.print();	    
+	    MyStack ms= new MyStack();
+		System.out.println(m.pop());
 	}
 
 }
